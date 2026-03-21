95ba0a82fe09ed9e3fc83f1202bbd090c0866c03
pritam <mandalpritam10@Mac.lan>
2026-03-21 22:11:26 +0530

    second assignment of day-03 is also completed successfully

---
+++ day-03/system_health_assignment.py
@@ -0,0 +1,45 @@
+import psutil
+
+def check_system_health():
+    try:
+        cpu_threshold=int(input("Enter the cpu threshold value(in percents): "))
+        disk_threshold=int(input("Enter the disk threshold value(in percents): "))
+        memory_threshold=int(input("Enter the memory threshold value(in percents): "))
+    
+    except ValueError:
+        print("\nPlease enter proper integer values")
+        return
+    
+    except KeyboardInterrupt:
+        print("\nProgram interrupted by user")
+        return
+    
+    except EOFError:
+        print("\nNo input received")
+        return
+
+    cpu_usage=psutil.cpu_percent(interval=1)
+    print(f"Current cpu usage is: {cpu_usage}%")
+    disk_usage=psutil.disk_usage('/').percent
+    print(f"Current disk usage is: {disk_usage}%")
+    memory_usage=psutil.virtual_memory().percent
+    print(f"Current memory usage is: {memory_usage}%")
+
+    if cpu_usage > cpu_threshold:
+        print("CPU alert")
+    else:
+        print("CPU running safely")
+
+    if disk_usage > disk_threshold:
+        print("disk alert")
+    else:
+        print("you can use the disk safely")
+    
+    if memory_usage > memory_threshold:
+        print("memory alert")
+    else:
+        print("you can use the memory safely")
+
+
+if __name__ == "__main__":
+    check_system_health()
