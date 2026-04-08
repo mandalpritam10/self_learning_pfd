c524cfe15fafa2fd300702a2e606bfe27e6a59c2
pritam <mandalpritam10@Mac.lan>
2026-04-09 03:19:56 +0530

    feat: log-analysis-agent project done successfully

--- projects/log-analysis-agent/logs_agent.py
+++ projects/log-analysis-agent/logs_agent.py
@@ -1,4 +1,4 @@
-from strands import Agent # Importing the Agentic Framework
+from strands import Agent  #Importing the Agentic Framework
 from strands_tools import file_read
 from strands.models.ollama import OllamaModel
 
@@ -6,18 +6,23 @@ SYSTEM_PROMPT = """
 You are a Log Analysis Agent.
 You are excellent in reading and understanding the log files i.e (.log / var/www/ /system log, etc)
 You can deduce results in short and crisp manner
-You are helpfule and use a DevOps Mindset in Log Analaysis and Root Cause Analysis
+You are helpful and use a DevOps Mindset in Log Analaysis and Root Cause Analysis
 You won't hallucinate and suggest new changes.
 You will not engage in any production actions, but suggest changes and ideas to DevOps Engineers.
 """
 
 ollama_model = OllamaModel(
-    host="http://localhost:11434",  # Ollama server address
-    model_id="llama3.2"               # Specify which model to use
+    host="http://localhost:11434", #Ollama server address
+    model_id="llama3.2"
 )
 
-agent = Agent(system_prompt=SYSTEM_PROMPT,
+agent = Agent(
+    system_prompt=SYSTEM_PROMPT,
     model=ollama_model,
-    tools=[file_read]) # Calling Bedrock LLMs
+    tools=[file_read])
 
-agent("Detect how many times the INFO, WARNING, ERROR occurs and return the counts only from app.log")
+agent("Detect how many times the INFO, WARNING, ERROR occurs and return the counts only from app.log file in the directory")
+
+
+# for counting never use agents, it will not give you exact answer, it will haallucinate, 
+# use agents for analysis, explanation, suggestion atc not for counting logs etc
